aac3d6d5d513ce29ac3cc3a5760499db1b23860b
akaregi <verdigris@azure.icu>
2023-03-23 22:53:45 +0900

    chore: clean
    Signed-off-by: akaregi <verdigris@azure.icu>

---
+++ src/pages/works.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "@/styles/Works.module.css";
+
+export default function Works() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main className={styles.main}>
+        <p>WORKS</p>
+      </main>
+    </>
+  );
+}
